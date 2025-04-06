dfc43b52147c65b3cff40b048767c4bca017eb17
MD: Miel Mahmud Sifat <mahmudsifat2793@gmail.com>
2025-04-06 12:20:31 +0600

    first commit

---
+++ bin/assignment -1.dart
@@ -0,0 +1,23 @@
+abstract class Vehicle {
+  int _speed = 0;
+
+  void move();
+
+  void setSpeed(int speed) {
+    _speed = speed;
+  }
+
+  int get speed => _speed;
+}
+
+class Car extends Vehicle {
+  void move() {
+    print('The car is moving at ${speed} km/h');
+  }
+}
+
+void main() {
+  Car myCar = Car();
+  myCar.setSpeed(1000448);
+  myCar.move();
+}
